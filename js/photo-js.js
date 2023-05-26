7bfb5e5da9c05eb7942603b594761a097fac3c98
Zoren Madridano <zorenvejerano.madridano@bicol-u.edu.ph>
2023-05-27 05:35:50 +0800

    Last Commit

--- js/photo-js.js
+++ js/photo-js.js
@@ -1,20 +1,18 @@
 const modal = document.querySelector('.modal');
 const previews = document.querySelectorAll(".gallery img");
 const original = document.querySelector(".full-img");
-const caption = document.querySelector(".caption");
 
 previews.forEach(preview => {
     preview.addEventListener('click', (e) => {
         console.log(e);
         modal.classList.add("open");
         original.classList.add("open");
+        
         //Dynamic change text and image
         const originalSrc = preview.getAttribute('data-original');
-        original.src = `./full/${originalSrc}`
-        const altText = preview.alt;
-        caption.textContent = altText;
+        original.src = `images/gallery/full-summer-pics/${originalSrc}`;
     })
-})
+});
 
 modal.addEventListener('click', (e) => {
     if(e.target.classList.contains("modal")){
